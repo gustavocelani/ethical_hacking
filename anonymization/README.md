289f3e468a73cf41871ddefeff8962b9895e27cc
Gustavo Pasqua de Oliveira Celani <gustavo_celani@hotmail.com>
2021-02-19 22:33:48 -0300

    Anonymization

---
+++ anonymization/README.md
@@ -0,0 +1,18 @@
+# Anonymization
+
+**Tor** is free and open-source software for enabling anonymous communication by directing Internet traffic through a free, worldwide, volunteer overlay network consisting of more than seven thousand relays in order to conceal a user's location and usage from anyone conducting network surveillance or traffic analysis.\
+\
+Using **Tor** makes it more difficult to trace the Internet activity to the user: this includes "visits to Web sites, online posts, instant messages, and other communication forms".\
+\
+Tor's intended use is to protect the personal privacy of its users, as well as their freedom and ability to conduct confidential communication by keeping their Internet activities unmonitored. 
+
+## Usage
+
+```
+$ git clone https://github.com/gustavocelani/ethical_hacking.git
+$ cd ./anonymization
+$ chmod +x anonymization.sh
+$ ./anonymization.sh
+```
+
+![Alt text](usage.png?raw=true "Usage")
---
+++ anonymization/anonymization.sh
@@ -0,0 +1,152 @@
+
+#####################################################################################
+#               Copyright (C) Gustavo Celani - All Rights Reserved
+#          Written by Gustavo Celani <gustavo_celani@hotmail.com>, 2021
+#####################################################################################
+#  Creation Date: 19/02/2021
+#  Creation Time: 21:10:43
+#
+#  Version:  1.0
+#  Revision: 1
+#
+#  Author:   Gustavo Pasqua de Oliveira Celani
+#  Social:   Gustavo Celani (@gustavo_celani)
+#  Email:    gustavo_celani@hotmail.com
+#  Linkedin: https://br.linkedin.com/in/gustavocelani
+#  GitHub:   https://github.com/gustavocelani
+#
+#####################################################################################
+
+#!/bin/bash
+
+_title_() {
+    echo -e "\n#################################################################"
+    figlet $1
+    echo -e "#################################################################\n"
+}
+
+_press_any_key_to_continue_() {
+    echo ""
+    read -n 1 -s -r -p "Press any key to continue..."
+}
+
+_validate_path_() {
+    if [[ "$(pwd)" != *anonymization ]]
+    then
+        echo -e "Please, call this script from anonymization directory..."
+        echo -e "                              -------------\n"
+        exit 1
+    fi
+}
+
+_Setup_() {
+
+    _title_ "Setup"
+
+    _validate_path_
+
+    sudo apt-get install -y torbrowser-launcher tor privoxy
+
+    echo -e "\nBackuping current privoxy config"
+    sudo cp -v /etc/privoxy/config /etc/privoxy/config.bpk
+
+    echo -e "\nAttempt to replace privoxy config"
+    sudo cp -v ./privoxy/config /etc/privoxy/config
+}
+
+_Start_() {
+
+    _title_ "Start"
+
+    echo -e "\nAttempt to start tor service"
+    sudo /etc/init.d/tor start
+
+    echo -e "\nAttempt to start privoxy service"
+    sudo /etc/init.d/privoxy start
+}
+
+_Stop_() {
+
+    _title_ "Stop"
+
+    echo -e "\nAttempt to stop tor service"
+    sudo /etc/init.d/tor stop
+
+    echo -e "\nAttempt to stop privoxy service"
+    sudo /etc/init.d/privoxy stop
+}
+
+_Status_() {
+
+    _title_ "Status"
+
+    echo -e "\nTor service status"
+    sudo /etc/init.d/tor status
+
+    echo -e "\nPrivoxy service status"
+    sudo /etc/init.d/privoxy status
+}
+
+_IP_Test_() {
+
+    _title_ "IP Test"
+
+    echo -e "\nReal Public IP Address:"
+    curl ifconfig.me
+
+    echo -e "\n\nAnonymized Public IP Address:"
+    torify curl ifconfig.me
+
+    echo ""
+}
+
+_IP_Info_() {
+
+    _title_ "IP Info"
+
+    echo -e "\nPlease, make shure that all firefox applications are closed...\n"
+    proxychains firefox https://ipleak.net/
+}
+
+_Launch_Tor_Browser_() {
+
+    _title_ "Launch Tor Browser"
+    torbrowser-launcher
+}
+
+_Exit_() {
+
+    _Stop_
+    echo ""
+    exit 0
+}
+
+#
+# Main Loop
+#
+
+while true
+do
+    clear
+    ACTION=$( dialog \
+            --stdout \
+            --backtitle "Powered by Gustavo Celani ( github.com/gustavocelani )" \
+            --title "Anonymization" \
+            --clear \
+            --ok-label "OK" \
+            --nocancel \
+            --menu "" \
+        0 0 0 \
+        _Setup_                "Initial Setup for Anonymization" \
+        _Start_                "Start Anonymization" \
+        _Stop_                 "Stop Anonymization" \
+        _Status_               "Anonymization Services Status" \
+        _IP_Test_              "Public IP Test" \
+        _IP_Info_              "Public IP Info" \
+        _Launch_Tor_Browser_   "Launch Tor Browser" \
+        _Exit_                 "Exit" )
+
+    clear
+    ${ACTION}
+    _press_any_key_to_continue_
+done
